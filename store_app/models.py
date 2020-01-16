5d8d62beea44ba609f188f4cb6bd373f19642979
nastinsk <nastinsk@gmail.com>
2020-01-16 00:49:07 -0800

    post, update, delete functionality added

--- store_app/models.py
+++ store_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 
 class Product(models.Model):
     title = models.CharField(max_length=200)
@@ -11,3 +11,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.id])
--- store_app/views.py
+++ store_app/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Product
 
@@ -11,3 +12,18 @@ class ProductListView(ListView):
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'product_detail.html'
+
+class ProductAddNew(CreateView):
+    model = Product
+    template_name = "product_new.html"
+    fields = ['title', 'description', 'manufacturer']
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = "product_update.html"
+    fields = ['title', 'description', 'manufacturer']
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = "product_delete.html"
+    success_url = reverse_lazy('home')
